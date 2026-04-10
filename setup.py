0fff43026f196098dc6d869bdb7eadd9d3649f7a
RahanaPA <rahanapgm@gmail.com>
2026-04-10 15:18:32 +0400

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "realtime-source-code-analyzer",
+    version = "0.1.0",
+    author= "Rahana",
+    author_email= "rahanapgm@gmail.com",
+    packages= find_packages(),
+    install_requires= []
+)
